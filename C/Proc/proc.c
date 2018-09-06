5db3fa87fca0d4e53dfa246e0a1f9fbed0147b35
AHAOAHA <ahaoaha_@outlook.com>
2018-09-06 23:39:43 +0800

    进度条

---
+++ C/Proc/proc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+	int i=0;
+	char bar[102]={'\0'};
+	char* buf="-\\|/";
+	while(i<=100){
+		bar[i]='#';
+		printf("[%-101s][%c][%d%%]\r",bar,buf[i%4],i);
+		fflush(stdout);
+		usleep(100000);
+		++i;
+	}
+	return 0;
+}
