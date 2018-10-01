69133d5e7660f28848068672c2ffbe0b20e141dc
AHAOAHA <ahaoaha_@outlook.com>
2018-10-01 22:21:04 +0800

    test

--- C/test/test.c
+++ C/test/test.c
@@ -1,31 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<unistd.h>
+//void Hello(int i)
+//{
+//    printf("hello Linux   %d\n",i);
+//}
+//int main()
+//{
+//    pid_t id=fork();
+//    if(-1==id)
+//    {
+//        printf("error\n");
+//        exit(1);
+//    }
+//    else if(0 == id)//子进程
+//    {
+//        while(1)
+//        {
+//            sleep(2);
+//        }
+//    }
+//    else
+//    {
+//        while(1)
+//        {
+//            sleep(1);
+//        }
+//    }
+//    return 0;
+//}
 #include<unistd.h>
-void Hello(int i)
-{
-    printf("hello Linux   %d\n",i);
-}
+#include<stdio.h>
 int main()
 {
-    pid_t id=fork();
-    if(-1==id)
-    {
-        printf("error\n");
-        exit(1);
-    }
-    else if(0 == id)//子进程
-    {
-        while(1)
-        {
-            sleep(2);
-        }
-    }
-    else
-    {
-        while(1)
-        {
-            sleep(1);
-        }
-    }
+
     return 0;
 }
